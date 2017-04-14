ae23e634166561c773edd19638ed861a3164d83f
anh.nguyen3 <anha04421@yahoo.com>
2017-04-14 13:24:23 +0700

    Test

--- main.go
+++ main.go
@@ -111,7 +111,7 @@ func init(){
 	}
 	exPath := path.Dir(ex)
 	fmt.Println(exPath)
-	file, err := os.OpenFile(exPath+log_file, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile(exPath+"/"+log_file, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln("Failed to open log file",  ":", err)
 	}
