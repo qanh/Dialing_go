5d2fbfe8a68ad093e5b1ab2fc50986949b055651
anh.nguyen3 <anha04421@yahoo.com>
2017-08-09 16:08:11 +0700

    fix bug

--- asterisk.go
+++ asterisk.go
@@ -782,6 +782,6 @@ func ast_channel(m map[string]string){
 	}
 }
 func checknumqueue(){
-	cmd,_ :=exec.Command("sudo /usr/sbin/asterisk -rx 'core show channels concise'|wc -l").Output()
-	fmt.Println(cmd)
+	cmd,err :=exec.Command("sudo /usr/sbin/asterisk -rx 'core show channels concise'|wc -l").Output()
+	fmt.Println(cmd,err)
 }
