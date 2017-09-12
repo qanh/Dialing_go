972c191eeb1b0683941bc61756b7bf746a00002c
anh.nguyen3 <anha04421@yahoo.com>
2017-09-12 11:06:20 +0700

    fix bug

--- asterisk.go
+++ asterisk.go
@@ -930,7 +930,7 @@ func ast_delete_peercache()(int,string){
 }
 
 func ast_check_meetme(peer string)(bool){
-	rs, _:=sh.Command("asterisk","-rx","confbridge list 8800"+peer+" concise").Command("grep","agent").Output()
+	rs, _:=sh.Command("asterisk","-rx","confbridge list 8800"+peer).Command("grep","agent").Output()
 	result:=string(rs)
 	plog(result,1)
 	if result!="" && !strings.Contains(result,"SIP/MAN"){
